04283ccc5d0974ed5e6199b1101c7ba36e6c0365
Yousef Elsayed <yousef@yousef-macbook-pro.local>
2023-04-14 18:44:45 +0400

    added argparser to take file path

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from converters import ITermToWindowsConverter
+import argparse
 import os
+
+parser = argparse.ArgumentParser(description='Iterm2 theme converter')
+
+# Add the argument
+parser.add_argument('path', type=str, help='Path to .itermcolors file')
+
+# Parse the arguments
+args = parser.parse_args()
 
 if __name__ == "__main__":
     for file in os.listdir('iterm2-themes'):
-        ITermToWindowsConverter.convert(f"iterm2-themes/{file}")
+        ITermToWindowsConverter.convert(f"{args.path}")
